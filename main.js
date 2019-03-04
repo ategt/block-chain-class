8b5d7de7d5bedaadc89a7e49f7cce9f033168cd5
Adam Tegtmeier <ategtmei@gmail.com>
2019-03-04 15:56:58 -0500

    Part 2 cleaned up.

--- main.js
+++ main.js
@@ -20,13 +20,6 @@ class Block {
         return hash.toString();
     }
 
-
-    doNothing() {
-        if (test.thig) {
-            this.doNothing();
-        }
-    }
-
     mineBlock(difficulty) {
         while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
             this.nonce++;
